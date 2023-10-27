f19317e2f02d239a5158ddd226abe1968be08f5a
Veildigital <78675311+Veildigital@users.noreply.github.com>
2023-10-27 23:04:02 +0800

    Changed password hashing algorithm from md5 to bcrypt for user

--- registration.php
+++ registration.php
@@ -1,84 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-<?php
-
-session_start(); //temp session
-error_reporting(0); // hide undefine index
-include("connection/connect.php"); // connection
-if(isset($_POST['submit'] )) //if submit btn is pressed
-{
-     if(empty($_POST['firstname']) ||  //fetching and find if its empty
-   	    empty($_POST['lastname'])|| 
-		empty($_POST['email']) ||  
-		empty($_POST['phone'])||
-		empty($_POST['password'])||
-		empty($_POST['cpassword']) ||
-		empty($_POST['cpassword']))
-		{
-			$message = "All fields must be Required!";
-		}
-	else
-	{
-		//cheching username & email if already present
-	$check_username= mysqli_query($db, "SELECT username FROM users where username = '".$_POST['username']."' ");
-	$check_email = mysqli_query($db, "SELECT email FROM users where email = '".$_POST['email']."' ");
-		
-
-	
-	if($_POST['password'] != $_POST['cpassword']){  //matching passwords
-       	$message = "Password not match";
-    }
-	elseif(strlen($_POST['password']) < 6)  //cal password length
-	{
-		$message = "Password Must be >=6";
-	}
-	elseif(strlen($_POST['phone']) < 10)  //cal phone length
-	{
-		$message = "invalid phone number!";
-	}
-
-    elseif (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) // Validate email address
-    {
-       	$message = "Invalid email address please type a valid email!";
-    }
-	elseif(mysqli_num_rows($check_username) > 0)  //check username
-     {
-    	$message = 'username Already exists!';
-     }
-	elseif(mysqli_num_rows($check_email) > 0) //check email
-     {
-    	$message = 'Email Already exists!';
-     }
-	else{
-       
-	 //inserting values into db
-	$mql = "INSERT INTO users(username,f_name,l_name,email,phone,password,address) VALUES('".$_POST['username']."','".$_POST['firstname']."','".$_POST['lastname']."','".$_POST['email']."','".$_POST['phone']."','".md5($_POST['password'])."','".$_POST['address']."')";
-	mysqli_query($db, $mql);
-		$success = "Account Created successfully! <p>You will be redirected in <span id='counter'>5</span> second(s).</p>
-														<script type='text/javascript'>
-														function countdown() {
-															var i = document.getElementById('counter');
-															if (parseInt(i.innerHTML)<=0) {
-																location.href = 'login.php';
-															}
-															i.innerHTML = parseInt(i.innerHTML)-1;
-														}
-														setInterval(function(){ countdown(); },1000);
-														</script>'";
-		
-		
-		
-		
-		 header("refresh:5;url=login.php"); // redireted once inserted success
-    }
-	}
-
-}
-
-
-?>
-
-
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -87,16 +8,93 @@ if(isset($_POST['submit'] )) //if submit btn is pressed
     <meta name="description" content="">
     <meta name="author" content="">
     <link rel="icon" href="#">
-    <title>Starter Template for Bootstrap</title>
+    <title>Registration</title>
     <!-- Bootstrap core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/font-awesome.min.css" rel="stylesheet">
     <link href="css/animsition.min.css" rel="stylesheet">
     <link href="css/animate.css" rel="stylesheet">
     <!-- Custom styles for this template -->
-    <link href="css/style.css" rel="stylesheet"> </head>
+    <link href="css/style.css" rel="stylesheet">
+</head>
+
 <body>
-     
+<?php
+session_start(); // temp session
+error_reporting(0); // hide undefined index errors
+include("connection/connect.php"); // connection to database
+
+if(isset($_POST['submit'] )) // if submit button was pressed
+{
+   if(empty($_POST['firstname']) ||  // fetching and find if its empty
+      empty($_POST['lastname']) || 
+      empty($_POST['email']) ||  
+      empty($_POST['phone']) ||
+      empty($_POST['password']) ||
+      empty($_POST['cpassword']))
+   {
+      $message = "All fields must are required to be filled!";
+   }
+
+	else
+	{
+		//cheching username & email if already present
+      $check_username = mysqli_query($db, "SELECT username FROM users where username = '".$_POST['username']."' ");
+      $check_email = mysqli_query($db, "SELECT email FROM users where email = '".$_POST['email']."' ");
+
+      if($_POST['password'] != $_POST['cpassword']) // matching passwords
+      {  
+         $message = "Passwords do not match!";
+      }
+
+      elseif(strlen($_POST['password']) < 6)  // cal password length
+      {
+         $message = "Passwords must be longer than 6 characters!";
+      }
+
+      elseif(strlen($_POST['phone']) < 10)  // cal phone length
+      {
+         $message = "Invalid phone number!";
+      }
+
+      elseif (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) // validate email address
+      {
+         $message = "Invalid email address, please type a valid email!";
+      }
+
+      elseif(mysqli_num_rows($check_username) > 0)  // check username
+      {
+         $message = 'Username already exists!';
+      }
+
+      elseif(mysqli_num_rows($check_email) > 0) // check email
+      {
+         $message = 'Email already exists!';
+      }
+
+      else
+      {
+	      //inserting values into db
+         $mql = "INSERT INTO users(username,f_name,l_name,email,phone,password,address) VALUES('".$_POST['username']."','".$_POST['firstname']."','".$_POST['lastname']."','".$_POST['email']."','".$_POST['phone']."','".password_hash($_POST['password'], PASSWORD_BCRYPT)."','".$_POST['address']."')";
+         mysqli_query($db, $mql);
+		
+         $success = "Account Created successfully! <p>You will be redirected in <span id='counter'>5</span> second(s).</p>
+                     <script type='text/javascript'>
+                        function countdown() {
+                           var i = document.getElementById('counter');
+                           if (parseInt(i.innerHTML)<=0) {
+                              location.href = 'login.php';
+                           }
+                           i.innerHTML = parseInt(i.innerHTML)-1;
+                        }
+                        setInterval(function(){ countdown(); },1000);
+                     </script>'";
+
+		   header("refresh:5;url=login.php"); // redirected once account created
+    }
+	}
+}
+?>
          <!--header starts-->
          <header id="header" class="header-scroll top-header headrom">
             <!-- .navbar -->
@@ -371,5 +369,4 @@ if(isset($_POST['submit'] )) //if submit btn is pressed
     <script src="js/headroom.js"></script>
     <script src="js/foodpicky.min.js"></script>
 </body>
-
 </html>

8886febb70289fa6e01281427d0cd96c2c15c3ba
GanJunYu <80407046+emtskw2m@users.noreply.github.com>
2023-10-27 13:20:35 +0800

    added buysell.php

---
+++ buysell.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Little Farmer Website</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="buysell/style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body class="w3-content">
+    <!-- Page Header -->
+    <header>
+        <a href="index.php">
+            <img src="buysell/img/logo.png" alt="logo">
+        </a>
+        <p class="header-text">Little Farmer Login</p>
+    </header>
+
+    <!-- First Grid -->
+    <div class="products-grid">
+        <a href="login.php">
+            <div class="product-card">
+                <img src="buysell/img/buyer.png" alt="buyer">
+                <p>Log In as a Buyer</p>
+            </div>
+        </a>
+
+        <a href="seller/index.php">
+            <div class="product-card">
+                <img src="buysell/img/seller.png" alt="seller">
+                <p>Log In as a Seller</p>
+            </div>
+        </a>
+    </div>
+
+</body>
+
+</html>

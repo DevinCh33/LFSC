fad56ea2812fd4c0cbf88b7e990f649d3a57f0eb
Veildigital <78675311+Veildigital@users.noreply.github.com>
2023-10-30 10:25:29 +0800

    Implemented log in checks for user side

--- delete_orders.php
+++ delete_orders.php
@@ -1,11 +1,9 @@
 <?php
-include("connection/connect.php"); //connection to db
+include("connection/connect.php"); // connection to db
 error_reporting(0);
 session_start();
 
-
 // sending query
-mysqli_query($db,"DELETE FROM users_orders WHERE o_id = '".$_GET['order_del']."'"); // deletig records on the bases of ID
-header("location:your_orders.php");  //once deleted success redireted back to current page
-
+mysqli_query($db,"DELETE FROM users_orders WHERE o_id = '".$_GET['order_del']."'"); // deleting records on the basis of ID
+header("location:your_orders.php");  // once delete success redirect back to current page
 ?>

68c6b53e928a79f134bce7bbf5e1c03a1f2b1b83
DevinCh33 <102984883+DevinCh33@users.noreply.github.com>
2023-11-03 18:36:22 +0800

    added "category_products.php" file for "categories"

--- category_products.php
+++ category_products.php
@@ -66,11 +66,11 @@ if (empty($_SESSION["user_id"])) // if not logged in
         <div class="hero-inner">
             <h1>Order Delivery & Take-Out </h1>
             <h5 class="font-white space-xs">Find merchants, specials, and coupons for free</h5>
-            <a href="category_products.php?categories_id=4" class="btn btn-info" role="button">All</a>
-            <a href="category_products.php?categories_id=5" class="btn btn-outline-info" role="button">Leafy Green</a>
-            <a href="category_products.php?categories_id=6" class="btn btn-outline-info" role="button">Root Vegetables</a>
-            <a href="category_products.php?categories_id=7" class="btn btn-outline-info" role="button">Pome Fruits</a>
-            <a href="category_products.php?categories_id=8" class="btn btn-outline-info" role="button">Other</a>
+            <a href="login.php" class="btn btn-info" role="button">All</a>
+            <a href="login.php" class="btn btn-outline-info" role="button">Leafy Green</a>
+            <a href="#link" class="btn btn-outline-info" role="button">Root Vegetables</a>
+            <a href="#link" class="btn btn-outline-info" role="button">Pome Fruits</a>
+            <a href="#link" class="btn btn-outline-info" role="button">Other</a>
             <!--<a href="login.php" class="btn btn-info" role="button">All</a>
             <a href="login.php" class="btn btn-outline-info" role="button">Salad</a>
             <a href="#link" class="btn btn-outline-info" role="button">Herbs</a>
@@ -117,7 +117,7 @@ if (empty($_SESSION["user_id"])) // if not logged in
                 {   
                     echo '<div class="col-xs-12 col-sm-6 col-md-4 food-item">
                             <div class="food-item-wrap">
-                                <div class="figure-wrap bg-image" data-image-src="seller/Res_img/dishes/'.$r['img'].'">
+                                <div class="figure-wrap bg-image" data-image-src="../assets/images/stock/'.$r['img'].'">
                                     <div class="distance"><i class="fa fa-pin"></i>1240m</div>
                                     <div class="rating pull-left"> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star-o"></i> </div>
                                     <div class="review pull-right"><a href="#">198 reviews</a> </div>
@@ -141,27 +141,32 @@ if (empty($_SESSION["user_id"])) // if not logged in
     <section class="popular">
         <div class="container">
             <div class="title text-xs-center m-b-30">
-                <h2>Little Farmer's Featured Products</h2>
-                <p class="lead">Fresh and pesticide free!</p>
+                <!--<h2>Little Farmer's Featured Products</h2>
+                <p class="lead">Fresh and pesticide free!</p>-->
             </div>
             <div class="row">
                 <?php 
+                 //fetching records from table and filter using html data-filter tag
+                        if(isset($_GET['id'])) {
+                            $id = $_GET['id'];
+                        }
+						$ress= mysqli_query($db,"select * from product where categories_id = $id");  
                 // fetch records from database to display popular first 12 products from database
-                $query_res= mysqli_query($db,"select * from dishes LIMIT 12"); 
+               
                 
-                while($r=mysqli_fetch_array($query_res))
+                while($r=mysqli_fetch_array($ress))
                 {   
                     echo '  <div class="col-xs-12 col-sm-6 col-md-4 food-item">
                             <div class="food-item-wrap">
-                                <div class="figure-wrap bg-image" data-image-src="seller/Res_img/dishes/'.$r['img'].'">
+                                <div class="figure-wrap bg-image" data-image-src="assets/images/stock/'.$r['img'].'">
                                     <div class="distance"><i class="fa fa-pin"></i>1240m</div>
                                     <div class="rating pull-left"> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star-o"></i> </div>
                                     <div class="review pull-right"><a href="#">198 reviews</a> </div>
                                 </div>
                                 <div class="content">
-                                    <h5><a href="dishes.php?res_id='.$r['rs_id'].'">'.$r['title'].'</a></h5>
-                                    <div class="product-name">'.$r['slogan'].'</div>
-                                    <div class="price-btn-block"> <span class="price">$'.$r['price'].'</span> <a href="dishes.php?res_id='.$r['rs_id'].'" class="btn theme-btn-dash pull-right">Order Now</a> </div>
+                                    <h5><a href="category_products.php?res_id='.$r['categories_id'].'">'.$r['product_name'].'</a></h5>
+                                 
+                                    <div class="price-btn-block"> <span class="price">$'.$r['price'].'</span> <a href="category_products.php?res_id='.$r['categories_id'].'" class="btn theme-btn-dash pull-right">Order Now</a> </div>
                                 </div>
                                 
                             </div>
@@ -172,111 +177,7 @@ if (empty($_SESSION["user_id"])) // if not logged in
         </div>
     </section>
     <!-- Popular block ends -->
-    <!-- Featured Merchants starts -->
-    <section class="featured-restaurants">
-        <div class="container">
-            <div class="row">
-                <div class="col-sm-4">
-                    <div class="title-block pull-left">
-                        <h4>Featured Merchants</h4> </div>
-                </div>
-                <div class="col-sm-8">
-                    <!-- Merchants filter nav starts -->
-                    <div class="restaurants-filter pull-right">
-                        <nav class="primary pull-left">
-                            <ul>
-                                <li><a href="#" class="selected" data-filter="*">all</a> </li>
-                                <?php 
-                                    // display categories here
-                                    $res = mysqli_query($db,"select * from res_category");
-
-                                    while($row=mysqli_fetch_array($res))
-                                    {
-                                        echo '<li><a href="#" data-filter=".'.$row['c_name'].'"> '.$row['c_name'].'</a> </li>';
-                                    }
-                                ?>
-                                
-                            </ul>
-                        </nav>
-                    </div>
-                    <!-- Merchants filter nav ends -->
-                </div>
-            </div>
-            <!-- Merchants listing starts -->
-            <div class="row">
-                <div class="restaurant-listing">
-                    <?php  //fetching records from table and filter using html data-filter tag
-                        $ress= mysqli_query($db,"select * from restaurant");
-
-                        while($rows=mysqli_fetch_array($ress))
-                        {
-                                // fetch records from res_category table according to catgory ID
-                                $query= mysqli_query($db,"select * from res_category where c_id='".$rows['c_id']."' ");
-                                    $rowss=mysqli_fetch_array($query);
-
-                                    echo ' <div class="col-xs-12 col-sm-12 col-md-6 single-restaurant all '.$rowss['c_name'].'">
-                                    <div class="restaurant-wrap">
-                                        <div class="row">
-                                            <div class="col-xs-12 col-sm-3 col-md-12 col-lg-3 text-xs-center">
-                                                <a class="restaurant-logo" href="dishes.php?res_id='.$rows['rs_id'].'" > <img src="seller/Res_img/'.$rows['image'].'" alt="Merchant logo"> </a>
-                                            </div>
-                                            <!--end:col -->
-                                            <div class="col-xs-12 col-sm-9 col-md-12 col-lg-9">
-                                                <h5><a href="dishes.php?res_id='.$rows['rs_id'].'" >'.$rows['title'].'</a></h5> <span>'.$rows['address'].'</span>
-                                                <div class="bottom-part">
-                                                    <div class="cost"><i class="fa fa-check"></i> Min $ 10,00</div>
-                                                    <div class="mins"><i class="fa fa-motorcycle"></i> 30 min</div>
-                                                    <div class="ratings"> <span>
-                                                            <i class="fa fa-star"></i>
-                                                            <i class="fa fa-star"></i>
-                                                            <i class="fa fa-star"></i>
-                                                            <i class="fa fa-star"></i>
-                                                            <i class="fa fa-star-o"></i>
-                                                        </span> (122) </div>
-                                                </div>
-                                            </div>
-                                            <!-- end:col -->
-                                        </div>
-                                        <!-- end:row -->
-                                    </div>
-                                    <!--end:Merchant wrap -->
-                                </div>';
-                        }
-                    ?>
-                </div>
-            </div>
-            <!-- Merchants listing ends -->
-            
-        </div>
-    </section>
-    <!-- Featured Merchants ends -->
-    <section class="app-section">
-        <div class="app-wrap">
-            <div class="container">
-                <div class="row text-img-block text-xs-left">
-                    <div class="container">
-                        <div class="col-xs-12 col-sm-5 right-image text-center">
-                            <figure> <img src="images/app.png" alt="Right Image" class="img-fluid"> </figure>
-                        </div>
-                        <div class="col-xs-12 col-sm-7 left-text">
-                            <h3>The Best Food Delivery App</h3>
-                            <p>Now you can make food happen pretty much wherever you are thanks to the free easy-to-use Food Delivery &amp; Takeout App.</p>
-                            <div class="social-btns">
-                                <a href="#" class="app-btn apple-button clearfix">
-                                    <div class="pull-left"><i class="fa fa-apple"></i> </div>
-                                    <div class="pull-right"> <span class="text">Available on the</span> <span class="text-2">App Store</span> </div>
-                                </a>
-                                <a href="#" class="app-btn android-button clearfix">
-                                    <div class="pull-left"><i class="fa fa-android"></i> </div>
-                                    <div class="pull-right"> <span class="text">Available on the</span> <span class="text-2">Play store</span> </div>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
+   
     <!-- start: FOOTER -->
     <footer class="footer">
         <div class="container">

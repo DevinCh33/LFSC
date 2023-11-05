e6876d53355cae3d86260a7a11d6755c4512fb6a
Elaine-tyl <128559784+Elaine-tyl@users.noreply.github.com>
2023-11-05 15:58:52 +0800

    tt

--- market.php
+++ market.php
@@ -187,16 +187,7 @@ if (empty($_SESSION["user_id"])) // if not logged in
             <div class="row top-footer">
                 <div class="col-xs-12 col-sm-3 footer-logo-block color-gray">
                     <a href="#"> <img class="img-rounded" style = "margin-bottom:50px;" src="landing/logo.png" alt="logo"> </a> </div>
-                <div class="col-xs-12 col-sm-2 about color-gray">
-                    <h5>About Us</h5>
-                    <ul>
-                        <li><a href="#">About us</a> </li>
-                        <li><a href="#">History</a> </li>
-                        <li><a href="#">Our Team</a> </li>
-                        <li><a href="#">We are hiring</a> </li>
-                    </ul>
-                </div>
-
+               
                 <div class="col-xs-12 col-sm-2 pages color-gray">
                     <h5>Pages</h5>
                     <ul>
@@ -216,10 +207,17 @@ if (empty($_SESSION["user_id"])) // if not logged in
                 </div>
                 
                 <div class="col-xs-12 col-sm-3 popular-locations color-gray">
-                <h5>Delivery Hours</h5>
-				<p>Mon - Fri: 8am - 8pm</p>
-				<p>Saturday: 9am - 7pm</p>
-				<p>Sunday: 9am - 8pm</p>
+                    <h5>Delivery Hours</h5>
+                    <p>Mon - Fri: 8am - 8pm</p>
+                    <p>Saturday: 9am - 7pm</p>
+                    <p>Sunday: 9am - 8pm</p>
+                </div>
+
+                
+
+                <div class="col-xs-12 col-sm-2 WhatsApp color-gray">
+                    <h5>Contact</h5>
+                    <p>WhatsApp:<a href="https://api.whatsapp.com/send?phone=60102170960">   +60102170960</a></p> 
                 </div>
             </div>
             <!-- top footer ends -->
@@ -227,10 +225,10 @@ if (empty($_SESSION["user_id"])) // if not logged in
             <div class="bottom-footer">
                 <div class="row">
                    
-                    <div class="col-xs-12 col-sm-4 address color-gray">
+                    <div class="col-xs-12 col-sm-6 address color-gray">
                         <h5>Address</h5>
-                        <p>Concept design of oline food order and deliveye,planned as restaurant directory</p>
-                        <h5>Phone: <a href="tel:+080000012222">080 000012 222</a></h5> </div>
+                        <p>AB102 Ground Floor Parcel 2586-1-9 Lorong Persiaran Bandar Baru Batu Kawa 3D Batu Kawah New Township Jalan Batu Kawa 93250 Kuching Sarawak</p></div>
+                        <!-- <h5>WhatsApp:</h5> <a href="https://api.whatsapp.com/send?phone=60102170960">   +60102170960</a> -->
                     
                 </div>
             </div>

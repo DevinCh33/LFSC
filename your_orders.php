86526592a608b0cdb94cbfc53b3a4355e1215dea
Elaine-tyl <128559784+Elaine-tyl@users.noreply.github.com>
2023-11-05 16:10:28 +0800

    header&footer

--- your_orders.php
+++ your_orders.php
@@ -138,26 +138,31 @@ only screen and (max-width: 760px),
 </head>
 
 <body>
-    <!--header starts-->
-    <header id="header" class="header-scroll top-header headrom">
+   <!--header starts-->
+   <header id="header" class="header-scroll top-header headrom" >
         <!-- .navbar -->
         <nav class="navbar navbar-dark">
-            <div class="container">
+            <div class="container" >
                 <button class="navbar-toggler hidden-lg-up" type="button" data-toggle="collapse" data-target="#mainNavbarCollapse">&#9776;</button>
-                <a class="navbar-brand" href="index.php"> <img class="img-rounded" src="images/food-picky-logo.png" alt=""> </a>
+                <a class="navbar-brand" href="index.php"> <img class="img-rounded" style = "height:50px;width:50px;" src="landing/logo.png" alt="logo"> </a>
                 <div class="collapse navbar-toggleable-md  float-lg-right" id="mainNavbarCollapse">
-                    <ul class="nav navbar-nav">
+                    <ul class="nav navbar-nav" style = "font-size:22px;">
                         <li class="nav-item"> <a class="nav-link active" href="market.php">Home <span class="sr-only">(current)</span></a> </li>
                         
                         <?php
-                            echo  '<li class="nav-item"><a href="index.php" class="nav-link active">Little Farmer</a> </li>';
-                            echo  '<li class="nav-item"><a href="restaurants.php" class="nav-link active">Merchants</a> </li>';
-                            echo  '<li class="nav-item"><a href="dishes.php" class="nav-link active">Product</a> </li>';
-                            echo  '<li class="nav-item"><a href="your_orders.php" class="nav-link active">Cart</a> </li>';
-                            echo  '<li class="nav-item"><a href="logout.php" class="nav-link active">Logout</a> </li>';
-                        ?>
+                            echo '<li class="nav-item"><a href="index.php" class="nav-link active">Little Farmer</a> </li>';
+                            echo '<li class="nav-item"><a href="restaurants.php" class="nav-link active">Merchants</a> </li>';
+                            echo '<li class="nav-item"><a href="dishes.php" class="nav-link active">Product</a> </li>';
+                            echo '<li class="nav-item"><a href="your_orders.php" class="nav-link active">Cart</a> </li>';
                             
-                    </ul>
+                            if(isset($_SESSION['adm_co']))
+                            {
+                                echo '<li class="nav-item"><a href="seller/dashboard.php" class="nav-link active">Dashboard</a> </li>';
+                            }
+
+                            echo '<li class="nav-item"><a href="logout.php" class="nav-link active">Logout</a> </li>';
+                         ?>     
+                    </ul>  
                 </div>
             </div>
         </nav>
@@ -296,127 +301,62 @@ else
                 </div>
             </div>
         </section>
-        <section class="app-section">
-            <div class="app-wrap">
-                <div class="container">
-                    <div class="row text-img-block text-xs-left">
-                        <div class="container">
-                            <div class="col-xs-12 col-sm-6 hidden-xs-down right-image text-center">
-                                <figure> <img src="images/app.png" alt="Right Image"> </figure>
-                            </div>
-                            <div class="col-xs-12 col-sm-6 left-text">
-                                <h3>The Best Food Delivery App</h3>
-                                <p>Now you can make food happen pretty much wherever you are thanks to the free easy-to-use Food Delivery &amp; Takeout App.</p>
-                                <div class="social-btns">
-                                    <a href="#" class="app-btn apple-button clearfix">
-                                        <div class="pull-left"><i class="fa fa-apple"></i> </div>
-                                        <div class="pull-right"> <span class="text">Available on the</span> <span class="text-2">App Store</span> </div>
-                                    </a>
-                                    <a href="#" class="app-btn android-button clearfix">
-                                        <div class="pull-left"><i class="fa fa-android"></i> </div>
-                                        <div class="pull-right"> <span class="text">Available on the</span> <span class="text-2">Play store</span> </div>
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+        <footer class="footer" style = "margin-top:10%;">
+        <div class="container">
+            <!-- top footer statrs -->
+            <div class="row top-footer">
+                <div class="col-xs-12 col-sm-3 footer-logo-block color-gray">
+                    <a href="#"> <img class="img-rounded" style = "margin-bottom:50px;" src="landing/logo.png" alt="logo"> </a> </div>
+               
+                <div class="col-xs-12 col-sm-2 pages color-gray">
+                    <h5>Pages</h5>
+                    <ul>
+                        <li><a href="market.php">Home</a> </li>
+                        <li><a href="index.php">Little Farmer</a> </li>
+                        <li><a href="restaurants.php">Merchants</a> </li>
+                        <li><a href="dishes.php">Product</a> </li>
+                        <li><a href="your_orders.php">Cart</a> </li>
+                    </ul>
+                </div>
+
+                <div class="col-xs-12 col-sm-2 how-it-works-links color-gray">
+                    <h5>Operating Hours</h5>
+                    <p>Mon - Fri: 8am - 8pm</p>
+                    <p>Saturday: 9am - 7pm</p>
+                    <p>Sunday: 9am - 8pm</p>
+                </div>
+                
+                <div class="col-xs-12 col-sm-3 popular-locations color-gray">
+                    <h5>Delivery Hours</h5>
+                    <p>Mon - Fri: 8am - 8pm</p>
+                    <p>Saturday: 9am - 7pm</p>
+                    <p>Sunday: 9am - 8pm</p>
+                </div>
+
+                
+
+                <div class="col-xs-12 col-sm-2 WhatsApp color-gray">
+                    <h5>Contact</h5>
+                    <p>WhatsApp:<a href="https://api.whatsapp.com/send?phone=60102170960">   +60102170960</a></p> 
                 </div>
             </div>
-        </section>
-        <!-- start: FOOTER -->
-        <footer class="footer">
-            <div class="container">
-                <!-- top footer statrs -->
-                <div class="row top-footer">
-                    <div class="col-xs-12 col-sm-3 footer-logo-block color-gray">
-                        <a href="#"> <img src="images/food-picky-logo.png" alt="Footer logo"> </a> <span>Order Delivery &amp; Take-Out </span> </div>
-                    <div class="col-xs-12 col-sm-2 about color-gray">
-                        <h5>About Us</h5>
-                        <ul>
-                            <li><a href="#">About us</a> </li>
-                            <li><a href="#">History</a> </li>
-                            <li><a href="#">Our Team</a> </li>
-                            <li><a href="#">We are hiring</a> </li>
-                        </ul>
-                    </div>
-                    <div class="col-xs-12 col-sm-2 how-it-works-links color-gray">
-                        <h5>How it Works</h5>
-                        <ul>
-                            <li><a href="#">Enter your location</a> </li>
-                            <li><a href="#">Choose restaurant</a> </li>
-                            <li><a href="#">Choose meal</a> </li>
-                            <li><a href="#">Pay via credit card</a> </li>
-                            <li><a href="#">Wait for delivery</a> </li>
-                        </ul>
-                    </div>
-                    <div class="col-xs-12 col-sm-2 pages color-gray">
-                        <h5>Pages</h5>
-                        <ul>
-                            <li><a href="#">Search results page</a> </li>
-                            <li><a href="#">User Sing Up Page</a> </li>
-                            <li><a href="#">Pricing page</a> </li>
-                            <li><a href="#">Make order</a> </li>
-                            <li><a href="#">Add to cart</a> </li>
-                        </ul>
-                    </div>
-                    <div class="col-xs-12 col-sm-3 popular-locations color-gray">
-                        <h5>Popular locations</h5>
-                        <ul>
-                            <li><a href="#">Sarajevo</a> </li>
-                            <li><a href="#">Split</a> </li>
-                            <li><a href="#">Tuzla</a> </li>
-                            <li><a href="#">Sibenik</a> </li>
-                            <li><a href="#">Zagreb</a> </li>
-                            <li><a href="#">Brcko</a> </li>
-                            <li><a href="#">Beograd</a> </li>
-                            <li><a href="#">New York</a> </li>
-                            <li><a href="#">Gradacac</a> </li>
-                            <li><a href="#">Los Angeles</a> </li>
-                        </ul>
-                    </div>
+            <!-- top footer ends -->
+            <!-- bottom footer statrs -->
+            <div class="bottom-footer">
+                <div class="row">
+                   
+                    <div class="col-xs-12 col-sm-6 address color-gray">
+                        <h5>Address</h5>
+                        <p>AB102 Ground Floor Parcel 2586-1-9 Lorong Persiaran Bandar Baru Batu Kawa 3D Batu Kawah New Township Jalan Batu Kawa 93250 Kuching Sarawak</p></div>
+                        <!-- <h5>WhatsApp:</h5> <a href="https://api.whatsapp.com/send?phone=60102170960">   +60102170960</a> -->
+                    
                 </div>
-                <!-- top footer ends -->
-                <!-- bottom footer statrs -->
-                <div class="row bottom-footer">
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-xs-12 col-sm-3 payment-options color-gray">
-                                <h5>Payment Options</h5>
-                                <ul>
-                                    <li>
-                                        <a href="#"> <img src="images/paypal.png" alt="Paypal"> </a>
-                                    </li>
-                                    <li>
-                                        <a href="#"> <img src="images/mastercard.png" alt="Mastercard"> </a>
-                                    </li>
-                                    <li>
-                                        <a href="#"> <img src="images/maestro.png" alt="Maestro"> </a>
-                                    </li>
-                                    <li>
-                                        <a href="#"> <img src="images/stripe.png" alt="Stripe"> </a>
-                                    </li>
-                                    <li>
-                                        <a href="#"> <img src="images/bitcoin.png" alt="Bitcoin"> </a>
-                                    </li>
-                                </ul>
-                            </div>
-                            <div class="col-xs-12 col-sm-4 address color-gray">
-                                <h5>Address</h5>
-                                <p>Concept design of oline food order and deliveye,planned as restaurant directory</p>
-                                <h5>Phone: <a href="tel:+080000012222">080 000012 222</a></h5> </div>
-                            <div class="col-xs-12 col-sm-5 additional-info color-gray">
-                                <h5>Addition informations</h5>
-                                <p>Join the thousands of other restaurants who benefit from having their menus on TakeOff</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <!-- bottom footer ends -->
             </div>
-        </footer>
-        <!-- end:Footer -->
-    </div>
-  
+            <!-- bottom footer ends -->
+        </div>
+    </footer>
+    <!-- end:Footer -->
+    
     <!-- Bootstrap core JavaScript
     ================================================== -->
     <script src="js/jquery.min.js"></script>
@@ -427,5 +367,6 @@ else
     <script src="js/jquery.isotope.min.js"></script>
     <script src="js/headroom.js"></script>
     <script src="js/foodpicky.min.js"></script>
+    <script src="js/cart.js"></script>
 </body>
 </html>

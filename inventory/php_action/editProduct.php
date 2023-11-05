eb08c47ed6903d5e964fd687dc93f33245d02b32
WcsWong <ryanwong179@gmail.com>
2023-11-05 16:53:55 +0800

    inventory product update
    Seller can add, edit, remove their own product

--- inventory/php_action/editProduct.php
+++ inventory/php_action/editProduct.php
@@ -12,7 +12,7 @@ if($_POST)
 	$categoryName 	= $_POST['editCategoryName'];
 	$productStatus 	= $_POST['editProductStatus'];
 		
-	$sql = "UPDATE product SET product_name = '$productName', categories_id = '$categoryName', quantity = '$quantity', price = '$price', active = '$productStatus', status = 1 WHERE product_id = $productId ";
+	$sql = "UPDATE product SET product_name = '$productName', categories_id = '$categoryName', quantity = '$quantity', price = '$price', active = '$productStatus', status = 1 WHERE product_id = $productId AND owner = '".$_SESSION['adm_id']."'";
 
 	if($db->query($sql) === TRUE) 
 	{

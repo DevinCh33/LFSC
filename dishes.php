817f353ce884d6674470c659dea8056a66ca32ab
GanJunYu <80407046+emtskw2m@users.noreply.github.com>
2023-11-19 02:22:45 +0800

    dishes.php update
    change the cart to right side as Dr.Sim suggestion

--- dishes.php
+++ dishes.php
@@ -93,13 +93,71 @@ if (empty($_SESSION["user_id"])) // if not logged in
             </div>
         </section>
         <!-- end:Inner page hero -->
+
         <div class="breadcrumb">
             <div class="container">
                 
             </div>
         </div>
+
         <div class="container m-t-30">
             <div class="row">
+                <!-- Column for POPULAR ORDERS -->
+                <div class="col-xs-12 col-sm-8 col-md-8 col-lg-9">
+                    <div class="menu-widget" id="2">
+                        <div class="widget-heading">
+                            <h3 class="widget-title text-dark">
+                                POPULAR ORDERS Quality products! <a class="btn btn-link pull-right" data-toggle="collapse" href="#popular2" aria-expanded="true">
+                                <i class="fa fa-angle-right pull-right"></i>
+                                <i class="fa fa-angle-down pull-right"></i>
+                                </a>
+                            </h3>
+                            <div class="clearfix"></div>
+                        </div>
+                
+                        <div class="collapse in" id="popular2">
+                        <?php // display values and item of products
+                            $stmt = $db->prepare("select * from product where owner = ".$_GET['res_id']);
+                            $stmt->execute();
+                            $products = $stmt->get_result();
+                            if (!empty($products)) 
+                            {
+                                foreach($products as $product)
+                                {           
+                        ?>
+
+                            <div class="food-item">
+                                <div class="row">
+                                    <div class="col-xs-12 col-sm-12 col-lg-8">
+                                        <div class="rest-logo pull-left">
+                                            <a class="restaurant-logo pull-left" href="#"><?php echo '<img src="'.$product['product_image'].'" alt="Product logo">'; ?></a>
+                                        </div>
+                                        <!-- end:Logo -->
+                                
+                                        <!-- end:col -->
+                                        <div class="col-xs-12 col-sm-12 col-lg-4 pull-right item-cart-info product" data-product-id="<?php echo $product['product_id']; ?>" data-product-owner="<?php echo $product['owner']; ?>"> 
+                                            <h6><?php echo $product['product_name']; ?></h6>
+                                            <span class="price pull-left" >RM <?php echo $product['price']; ?></span>
+                                            <input class="b-r-0" type="text" name="quantity" style="margin-left:30px;" value="1" size="2" />
+                                            <button class="btn theme-btn addsToCart" style="margin-left:40px;">Add to Cart</button>
+                                        </div>
+                                    </div>
+                                </div>
+                                <!-- end:row -->
+                            </div>
+                            <!-- end:Item -->
+                            
+                            <?php
+                                    }
+                                }
+                            ?>
+                        </div>
+                        <!-- end:Collapse -->
+                    </div>
+                    <!-- end:Widget menu -->
+                </div>
+
+                <!-- Column for Your Shopping Cart -->
                 <div class="col-xs-12 col-sm-4 col-md-4 col-lg-3">
                     <div class="widget widget-cart">
                         <div class="widget-heading">
@@ -121,7 +179,7 @@ if (empty($_SESSION["user_id"])) // if not logged in
                         <div class="widget-body">
                             <div class="price-wrap text-xs-center">
                                 <p>TOTAL</p>
-                                <h3 id="cartTotal" class="value">Total Price: RM 0.00</h3>
+                                <h3 id="cartTotal" class="value">RM 0.00</h3>
                                 <p>Free Shipping</p>
                                 <a href="checkout.php?res_id=<?php echo $_GET['res_id'];?>&action=check"  class="btn theme-btn btn-lg">Checkout</a>
                             </div>
@@ -129,66 +187,17 @@ if (empty($_SESSION["user_id"])) // if not logged in
                     </div>
                 </div>
 
-                <div class="col-xs-12 col-sm-8 col-md-8 col-lg-6">
-                    
-                <!-- end:Widget menu -->
-                <div class="menu-widget" id="2">
-                    <div class="widget-heading">
-                        <h3 class="widget-title text-dark">
-                            POPULAR ORDERS Quality products! <a class="btn btn-link pull-right" data-toggle="collapse" href="#popular2" aria-expanded="true">
-                            <i class="fa fa-angle-right pull-right"></i>
-                            <i class="fa fa-angle-down pull-right"></i>
-                            </a>
-                        </h3>
-                        <div class="clearfix"></div>
-                    </div>
-                    <div class="collapse in" id="popular2">
-                    <?php // display values and item of products
-                        $stmt = $db->prepare("select * from product where owner = ".$_GET['res_id']);
-                        $stmt->execute();
-                        $products = $stmt->get_result();
-                        if (!empty($products)) 
-                        {
-                            foreach($products as $product)
-                            {           
-                    ?>
-
-                        <div class="food-item">
-                            <div class="row">
-                                <div class="col-xs-12 col-sm-12 col-lg-8">
-                                    <div class="rest-logo pull-left">
-                                        <a class="restaurant-logo pull-left" href="#"><?php echo '<img src="'.$product['product_image'].'" alt="Product logo">'; ?></a>
-                                    </div>
-                                    <!-- end:Logo -->
-                            
-                                    <!-- end:col -->
-                                    <div class="col-xs-12 col-sm-12 col-lg-4 pull-right item-cart-info product" data-product-id="<?php echo $product['product_id']; ?>" data-product-owner="<?php echo $product['owner']; ?>"> 
-                                        <h6><?php echo $product['product_name']; ?></h6>
-                                        <span class="price pull-left" >RM <?php echo $product['price']; ?></span>
-                                        <input class="b-r-0" type="text" name="quantity" style="margin-left:30px;" value="1" size="2" />
-                                        <button class="btn theme-btn addsToCart" style="margin-left:40px;">Add to Cart</button>
-                                    </div>
-                                </div>
-                            </div>
-                            <!-- end:row -->
-                        </div>
-                        <!-- end:Item -->
-                        
-                        <?php
-                                }
-                            }
-                        ?>
-                    </div>
-                    <!-- end:Collapse -->
-                </div>
-                <!-- end:Widget menu -->
-                </div>
-                <!-- end:Bar -->
             </div>
             <!-- end:row -->
         </div>
         <!-- end:Container -->
 
+        <div class="breadcrumb">
+            <div class="container">
+                
+            </div>
+        </div>
+        
     <!-- start: FOOTER -->
     <?php
     include("includes/footer.php");

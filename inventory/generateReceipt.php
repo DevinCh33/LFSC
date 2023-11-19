c5b89ca2efc8c513cf3c3cc3c9864d3fb35c3fae
Elaine-tyl <128559784+Elaine-tyl@users.noreply.github.com>
2023-11-19 16:00:43 +0800

    generatereceipt

---
+++ inventory/generateReceipt.php
@@ -0,0 +1,47 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Retrieve start and end dates from the AJAX request
+    $startDate = $_POST["startDate"];
+    $endDate = $_POST["endDate"];
+
+    // Mock data for the report
+    $reportData = [
+        ['order_date' => '2023-01-01', 'product_name' => 'Product A', 'quantity' => 2, 'price' => 25.00],
+        ['order_date' => '2023-01-02', 'product_name' => 'Product B', 'quantity' => 1, 'price' => 15.00],
+    ];
+
+    // Generate receipt content based on the report
+    $receiptContent = "<div class='receipt-container'>";
+    $receiptContent .= "<h2>Payment Receipt</h2>";
+    $receiptContent .= "<p><strong>Date:</strong> " . date("Y-m-d H:i:s") . "</p>";
+    $receiptContent .= "<p><strong>Period:</strong> $startDate to $endDate</p>";
+    $receiptContent .= "<table class='table'>";
+    $receiptContent .= "<thead><tr><th>Order Date</th><th>Product Name</th><th>Quantity</th><th>Price</th></tr></thead><tbody>";
+
+    $totalAmount = 0;
+
+    foreach ($reportData as $order) {
+        $orderDate = $order["order_date"];
+        $productName = $order["product_name"];
+        $quantity = $order["quantity"];
+        $price = $order["price"];
+        $totalAmount += $price;
+
+        // Add order details to the receipt
+        $receiptContent .= "<tr>";
+        $receiptContent .= "<td>$orderDate</td>";
+        $receiptContent .= "<td>$productName</td>";
+        $receiptContent .= "<td>$quantity</td>";
+        $receiptContent .= "<td>$price</td>";
+        $receiptContent .= "</tr>";
+    }
+
+    $receiptContent .= "</tbody></table>";
+    $receiptContent .= "<p><strong>Total Amount:</strong> $totalAmount</p>";
+    $receiptContent .= "<p>Thank you for your payment!</p>";
+    $receiptContent .= "</div>";
+
+    // Return the receipt content as HTML
+    echo $receiptContent;
+}
+?>
--- inventory/report.php
+++ inventory/report.php
@@ -1,42 +1,94 @@
 <?php require_once 'includes/header.php'; ?>
 
 <div class="row">
-	<div class="col-md-12">
-		<div class="panel panel-default">
-			<div class="panel-heading">
-				<i class="glyphicon glyphicon-check"></i>	Order Report
-			</div>
-			<!-- /panel-heading -->
-			<div class="panel-body">
-				
-				<form class="form-horizontal" action="php_action/getOrderReport.php" method="post" id="getOrderReportForm">
-				  <div class="form-group">
-				    <label for="startDate" class="col-sm-2 control-label">Start Date</label>
-				    <div class="col-sm-10">
-				      <input type="text" class="form-control" id="startDate" name="startDate" placeholder="Start Date" />
-				    </div>
-				  </div>
-				  <div class="form-group">
-				    <label for="endDate" class="col-sm-2 control-label">End Date</label>
-				    <div class="col-sm-10">
-				      <input type="text" class="form-control" id="endDate" name="endDate" placeholder="End Date" />
-				    </div>
-				  </div>
-				  <div class="form-group">
-				    <div class="col-sm-offset-2 col-sm-10">
-				      <button type="submit" class="btn btn-success" id="generateReportBtn"> <i class="glyphicon glyphicon-ok-sign"></i> Generate Report</button>
-				    </div>
-				  </div>
-				</form>
+    <div class="col-md-12">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <i class="glyphicon glyphicon-check"></i> Order Report
+            </div>
+            <!-- /panel-heading -->
+            <div class="panel-body">
 
-			</div>
-			<!-- /panel-body -->
-		</div>
-	</div>
-	<!-- /col-dm-12 -->
+                <form class="form-horizontal" action="php_action/getOrderReport.php" method="post" id="getOrderReportForm">
+                    <div class="form-group">
+                        <label for="startDate" class="col-sm-2 control-label">Start Date</label>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" id="startDate" name="startDate" placeholder="Start Date" />
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="endDate" class="col-sm-2 control-label">End Date</label>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" id="endDate" name="endDate" placeholder="End Date" />
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <div class="col-sm-offset-2 col-sm-10">
+                            <button type="submit" class="btn btn-success" id="generateReportBtn"> <i class="glyphicon glyphicon-ok-sign"></i> Generate Report</button>
+                        </div>
+                    </div>
+                </form>
+
+            </div>
+            <!-- /panel-body -->
+        </div>
+    </div>
+    <!-- /col-dm-12 -->
 </div>
 <!-- /row -->
 
+<!-- Generated report content container -->
+<div id="generatedReportContainer"></div>
+
+<!-- Include jQuery -->
+<script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
 <script src="custom/js/report.js"></script>
+<script>
+    $(document).ready(function () {
+        // Add an event listener for the form submission
+        $("#getOrderReportForm").submit(function (event) {
+            event.preventDefault(); // Prevent the default form submission
+
+            // Perform AJAX request to generate the report
+            $.ajax({
+                url: $(this).attr("action"),
+                type: $(this).attr("method"),
+                data: $(this).serialize(),
+                dataType: 'html',
+                success: function (response) {
+                    // Display the generated report content
+                    $('#generatedReportContainer').html(response);
+
+                    // Create "Generate Receipt" button
+                    var generateReceiptButton = $('<button>', {
+                        class: 'btn btn-primary',
+                        html: '<i class="glyphicon glyphicon-list-alt"></i> Generate Receipt',
+                        id: 'generateReceiptBtn'
+                    });
+
+                    // Append the button to the report container
+                    $('#generatedReportContainer').append(generateReceiptButton);
+
+                    // Add click event listener to the "Generate Receipt" button
+                    generateReceiptButton.on('click', function () {
+                        console.log('Generate Receipt button clicked'); // Add this line for debugging
+
+                        // Perform AJAX request to generate the receipt
+                        $.ajax({
+                            url: 'generateReceipt.php',
+                            type: 'POST',
+                            data: $('#getOrderReportForm').serialize(),
+                            dataType: 'html',
+                            success: function (receiptContent) {
+                                // Display the receipt content on the page
+                                $('#generatedReportContainer').append('<div class="receipt-content">' + receiptContent + '</div>');
+                            }
+                        });
+                    });
+                }
+            });
+        });
+    });
+</script>
 
 <?php require_once 'includes/footer.php'; ?>

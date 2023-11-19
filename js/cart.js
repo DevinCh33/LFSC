be2fe39d3e4d309d7b98a3a71ba8a4852263ecce
GanJunYu <80407046+emtskw2m@users.noreply.github.com>
2023-11-19 16:54:11 +0800

    fix some UI

--- js/cart.js
+++ js/cart.js
@@ -104,7 +104,7 @@ $(document).ready(function() {
                 <li>
                     <img src="${item.image}" alt="${item.name}" style="width:50px; height:50px;"><br/>
                     <a href="dishes.php?res_id=${item.owner}">${item.name}</a> - 
-                    RM ${item.price.toFixed(2)} x <input type="number" class="product quantity" style="max-width: 4rem;" value="${item.quantity}" min="0" data-product-id="${item.id}"/>
+                    RM ${item.price.toFixed(2)} x <br> <input type="number" class="product quantity" style="max-width: 4rem;" value="${item.quantity}" min="0" data-product-id="${item.id}"/>
                     <button class="product removeProduct" data-product-id="${item.id}">
                         <i class="fa fa-trash pull-right">Remove</i>
                     </button>

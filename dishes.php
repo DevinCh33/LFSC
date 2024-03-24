bda493bc1aafc31e287d5e55169daaa8c23a46fa
DevinCh33 <102984883+DevinCh33@users.noreply.github.com>
2024-03-24 16:55:06 +0800

    still working on comments
    still working on comments

--- dishes.php
+++ dishes.php
@@ -186,13 +186,13 @@ if (empty($_SESSION["user_id"])) // if not logged in
                         </div>
                         <div class="order-row bg-white">
                             <div class="widget-body">   
-                                <form id="commentForm" method="post">
-                                    <div class="form-group">
-                                        <label for="comment">Leave a comment:</label>
-                                        <textarea class="form-control" rows="3" id="comment" name="comment"></textarea>
-                                    </div>
-                                    <button type="submit" class="btn theme-btn btn-lg">Submit</button>
-                                </form>
+                            <form id="commentForm" method="post">
+    <div class="form-group">
+        <label for="comment">Leave a comment:</label>
+        <textarea class="form-control" rows="3" id="comment" name="comment"></textarea>
+    </div>
+    <button type="submit" class="btn theme-btn btn-lg">Submit</button>
+</form>
                             </div>
                         </div>
                         <!-- end:Order row -->
@@ -267,6 +267,6 @@ if (empty($_SESSION["user_id"])) // if not logged in
     <script src="js/foodpicky.min.js"></script>
     <script src="js/cart.js"></script>
     <script src="js/rating1.js"></script>
-    <script src="js/comments.js"></script>
+    <script src="js/comment.js"></script>
 </body>
 </html>
